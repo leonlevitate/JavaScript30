b61aed2249f53824936b916734394cb48b5de910
leon.linton@live.co.uk <leon.linton@live.co.uk>
2019-10-13 20:23:36 +0100

    Adds paintToCanvas function

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -15,3 +15,17 @@ function getVideo() {
       console.error(`OH NO!!!`, err);
     });
 }
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+    let pixels = ctx.getImageData(0, 0, width, height);
+    pixels = rgbSplit(pixels);
+    ctx.putImageData(pixels, 0, 0);
+  }, 16);
+}
