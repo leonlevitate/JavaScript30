9fc618001741e3bf7a57d7b70d9fab6b03ed16dd
leon.linton@live.co.uk <leon.linton@live.co.uk>
2019-10-13 20:24:33 +0100

    Adds takePhoto function

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -29,3 +29,14 @@ function paintToCanvas() {
     ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
+
+function takePhoto() {
+  snap.currentTime = 0;
+  snap.play();
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'handsome');
+  link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
+  strip.insertBefore(link, strip.firstChild);
+}
